7b6457cf6986d9bdaa3ea03d7e304870c1ce8725
Alessandro Benoit <micc83@gmail.com>
2013-06-12 22:35:03 +0300

    Update example.php

--- example.php
+++ example.php
@@ -14,7 +14,7 @@ $twitter_api = new Wp_Twitter_Api( $credentials );
 
 // Example a - Retrieve last 5 tweets from my timeline (default type statuses/user_timeline)
 $query = 'count=5&include_entities=true&include_rts=true&screen_name=micc1983';
-var_dump( $twitter_api->query( $query );
+var_dump( $twitter_api->query( $query ) );
 
 // Example b - Retrieve my follower with a cache of 24 hour (default 30 minutes)
 $query = 'screen_name=micc1983';
